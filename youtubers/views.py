d33656962bd1eb27ab2c334b86871013c6b1b41d
jenishVadodaria <jenish.d.v@gmail.com>
2022-02-22 16:30:40 +0530

    Initial commit

---
+++ youtubers/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def youtubers(request):
+    pass
+
+
+def youtubers_detail(request):
+    pass
+
+
+def search(request):
+    pass
